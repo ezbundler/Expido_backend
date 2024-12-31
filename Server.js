06eb50491ce82f8b03cd4ddab19e3b42295f2799
ezbundler <developer.akash1236@gmail.com>
2024-12-31 19:33:13 +0530

    initial setup

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+})
+
+
+
+
+
+
+// E0rTXFV4NFjzgZDa
+
+
+
+// mongodb+srv://developerakash1236:<db_password>@cluster0.qpsfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
